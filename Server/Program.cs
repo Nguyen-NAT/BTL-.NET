48f60e2392f9119cc6c7a243829504d602881018
NAT <88333445+Nguyen-NAT@users.noreply.github.com>
2026-04-13 19:35:15 +0700

    first commit

---
+++ Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows;
+
+namespace Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+    
+}
