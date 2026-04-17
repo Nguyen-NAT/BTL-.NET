0ca3599a8ab6453e665dcc616a2445733902c5aa
SeeleFuai <anhtungn749@gmail.com>
2026-04-17 17:42:44 +0700

    Add: Gitignore update

--- Client/obj/Debug/net10.0-windows/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net10.0-windows/Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b5743e596d5105ea1398f675a49ebfda3dec8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd696716964e4eae672dec5670a203807588d7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net10.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b5743e596d5105ea1398f675a49ebfda3dec8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd696716964e4eae672dec5670a203807588d7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
